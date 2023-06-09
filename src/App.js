01225edfb12040e51d69ce07625d3964323580ee
jaredjj3 <jared@jaredjohnson.dev>
2023-06-09 16:58:55 -0400

    Make Timer skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./style.css";
+import { Timer } from './Timer';
 
 export default function App() {
   return (
     <div>
       <h1>Speedrun Timer</h1>
-      <div>TODO: Timer</div>
+      <Timer />
+
+      <hr />
+      
       <div>TODO: Times</div>
     </div>
   );
---
+++ src/Timer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Timer = () => {
+  return (
+    <div>
+      <h2>TODO: Current time</h2>
+      <button>Start</button>
+      <button>Stop</button>
+      <button>Reset</button>
+    </div>
+  );
+};
