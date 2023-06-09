81c5e6d2e2bdaa4c64a8ae980902f11ca8411d1e
jaredjj3 <jared@jaredjohnson.dev>
2023-06-09 17:31:22 -0400

    Wire click handlers for the <Timer> buttons

--- src/Timer.js
+++ src/Timer.js
@@ -4,17 +4,37 @@ export const Timer = (props) => {
   const [elapsedTimeMs, setElapsedTimeMs] = useState(0);
   const onStop = props.onStop;
 
+  const onStartClick = () => {
+    console.log('onStartClick');
+  };
+
+  const onStopClick = () => {
+    console.log('onStopClick');
+  };
+
+  const onResetClick = () => {
+    console.log('onResetClick');
+  };
+
   return (
     <div>
       <h2>{elapsedTimeMs} ms</h2>
       <div className="btn-group" role="group" aria-label="Basic example">
-        <button type="button" className="btn btn-success">
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={onStartClick}
+        >
           start
         </button>
-        <button type="button" className="btn btn-danger">
+        <button type="button" className="btn btn-danger" onClick={onStopClick}>
           stop
         </button>
-        <button type="button" className="btn btn-warning">
+        <button
+          type="button"
+          className="btn btn-warning"
+          onClick={onResetClick}
+        >
           reset
         </button>
       </div>
